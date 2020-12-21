0a605eec54730d1ec49fc4194d829f9c622c7018
namseungkyung <tmdrud41@gmail.com>
2020-12-21 23:34:58 +0900

    회원가입, 아이디/비밀번호 찾기 화면 추가

---
+++ app/src/main/java/com/example/woozoosun_project/JoinActivity.java
@@ -0,0 +1,11 @@
+package com.example.woozoosun_project;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
---
+++ app/src/main/java/com/example/woozoosun_project/SearchPWActivity.java
@@ -0,0 +1,13 @@
+package com.example.woozoosun_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchPWActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_pw);
+    }
+}
