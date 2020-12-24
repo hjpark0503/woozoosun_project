017775f5de556d2e4501aef27fc311a86565221f
salduf <04fud27@gmail.com>
2020-12-24 12:22:41 +0900

    DB 실행 가능

--- app/src/main/java/com/example/woozoosun_project/HomeActivity.java
+++ app/src/main/java/com/example/woozoosun_project/HomeActivity.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,10 +24,25 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        /* 친구이름 리스트
-        Friends friend = new Friends("ryeong"); //""안에 사용자 id 넣을것
-        ArrayList friends = friend.showList(); //"ryeong"의 친구들이 리스트로 리턴됨
-        이 메소드 활용해서 친구들 이름 바꿔서 띄우면 됩니다!
+        /*
+        //Return_info_DB친구 정보 리턴
+        Return_info_DB info = new Return_info_DB("친구이름");
+        info.start();
+        while(info.flag == false){}
+        이름 = get_name()
+        아이디 = get_id()
+        폰 = get_phone()
+        주소 = info.get_address(); //리스트로 리턴 됨 .get(0~2)
+        //주소 빼고 나머지 다 string임
+         */
+
+        /*
+        //Friends_DB 친구이름 리스트 (수정)
+        Friends_DB friends = new Friends_DB("사용자 id");
+        friends.start();
+        while(friends.flag == false){}
+        List list = friends.list;
+        //list에 친구목록 담겨있음 사용자 id 외에 나머지는 바꿀필요 X
         */
 
         addressbtn1 = findViewById(R.id.addressbtn1);
