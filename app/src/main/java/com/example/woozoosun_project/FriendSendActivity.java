0b0431f53340d8eab6e1b3daa83c5e4ea9b1c51d
hyunjin <hjpark0503@naver.com>
2020-12-26 02:50:07 +0900

    2020-12-26 AM 2:48 회원가입 중복확인, 사용자 이름, 주소, 친구 목록, 친구 주소 등 완료 (여태까지 중에 최종)

--- app/src/main/java/com/example/woozoosun_project/FriendSendActivity.java
+++ app/src/main/java/com/example/woozoosun_project/FriendSendActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class FriendSendActivity extends AppCompatActivity {
 
-    String friendName;
-    List<String> addressList;
+    String name; //친구 이름
+    TextView selectFriendName;
+    List<String> addressList; //친구의 주소목록
+
+    Button[] friendAddressBtn;
+    TextView friendAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +26,53 @@ public class FriendSendActivity extends AppCompatActivity {
         setContentView(R.layout.activity_friend_send);
 
         Intent intent = getIntent();
-        friendName = intent.getStringExtra("name"); //친구 이름
-        addressList = (List<String>) intent.getSerializableExtra("addressList");
+        name = intent.getStringExtra("name"); //친구 이름
+
+        selectFriendName = findViewById(R.id.selectFriendName);
+        selectFriendName.setText("친구 "+name+" 님의 우주선");
+
+        //Return_info_DB친구 정보 리턴
+        Return_info_DB info = new Return_info_DB(name); //친구 정보 객체 생성
+        info.start();
+        while(info.flag == false){}
+        addressList = info.get_address(); //리스트로 리턴 됨 .get(0~2) 친구의 주소목록
+
+
+        friendAddressBtn = new Button[3];
+        friendAddressBtn[0] = findViewById(R.id.friendAddressBtn1);
+        friendAddressBtn[1] = findViewById(R.id.friendAddressBtn2);
+        friendAddressBtn[2] = findViewById(R.id.friendAddressBtn3);
+
+        //Return_info_DB친구 정보 리턴
+        Return_info_DB friendInfo = new Return_info_DB(name); //사용자 정보 객체 생성
+        friendInfo.start();
+        while(friendInfo.flag == false){}
+        List<String> addressList = friendInfo.get_address(); //리스트로 리턴 됨 .get(0~2)
+
+
+        friendAddress = findViewById(R.id.friendAddress);
+        friendAddress.setText(addressList.get(0)); //디폴트로 첫번째 주소로 보여주기
+
+        for(int i=0; i<3; i++){ //주소는 최대 3개까지
+            String address = addressList.get(i);
+            System.out.println("주소"+address);
+            Log.d("주소",address);
+            friendAddressBtn[i].setVisibility(View.VISIBLE);
+            friendAddressBtn[i].setText(address);
+
+            friendAddressBtn[i].setOnClickListener(new View.OnClickListener() {  //주소 버튼 눌렀을 때
+                @Override
+                public void onClick(View v) {
+                    friendAddress.setText(address);
+                }
+            });
+
+            if(i+1 != 3){ //get(i+1) 오버플로우 안나기 위해서
+                if(addressList.get(i+1).equals("null")){ //다음 주소가 null이면
+                    break;
+                }
+            }
+        }
 
 
     }
